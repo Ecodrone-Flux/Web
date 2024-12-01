d2f17aea250428db4836d71f3bd806ac4849dc76
Laura Nuñez <njm0031533@est.univalle.edu>
2024-12-01 05:20:36 -0400

    El mapa toma las alertas para crear markers

--- src/Components/WorldMapComponent.js
+++ src/Components/WorldMapComponent.js
@@ -1,23 +1,35 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { GoogleMap, Marker } from '@react-google-maps/api';
 import { useGoogleMaps } from './GoogleMapsContext';
 
 const WorldMapComponent = () => {
-  const markers = [
-    { coords: { lat: 31.230391, lng: 121.473701 }, name: "Shanghai" },
-    { coords: { lat: 28.704060, lng: 77.102493 }, name: "Delhi" },
-  ];
-
   const initialCenter = { lat: 20, lng: 0 }; // Coordenadas iniciales
   const initialZoom = 2; // Nivel de zoom inicial
 
   const [zoom, setZoom] = useState(initialZoom); // Estado para manejar el nivel de zoom
   const [center, setCenter] = useState(initialCenter); // Estado para manejar el centro del mapa
+  const [alerts, setAlerts] = useState([]); // Estado para manejar las alertas
 
   const mapRef = useRef(); // Ref para almacenar la instancia del mapa
 
   // Usamos el hook del contexto para verificar si la API de Google Maps se ha cargado
   const { isLoaded } = useGoogleMaps();
+
+  // Obtener las alertas desde el servidor
+  useEffect(() => {
+    const fetchAlerts = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/alerts");
+        const data = await response.json();
+        setAlerts(data); // Almacena las alertas en el estado
+        console.log(data);
+      } catch (err) {
+        console.error("Error al obtener las alertas:", err);
+      }
+    };
+
+    fetchAlerts();
+  }, []); // Se ejecuta solo una vez cuando el componente se monta
 
   const handleMarkerClick = (coords) => {
     setCenter(coords); // Cambia el centro del mapa al marcador clickeado
@@ -63,14 +75,15 @@ const WorldMapComponent = () => {
         }}
         options={{
           minZoom: initialZoom, // Nivel mínimo de zoom directamente en las opciones del mapa
+          mapTypeId: 'hybrid',
         }}
       >
-        {markers.map((marker) => (
+        {alerts.map((alert) => (
           <Marker
-            key={marker.name} // Asegúrate de que la clave del marcador sea única
-            position={marker.coords}
-            title={marker.name}
-            onClick={() => handleMarkerClick(marker.coords)} // Llama a la función handleMarkerClick cuando se hace clic
+            key={alert.id} // Asegúrate de que la clave del marcador sea única
+            position={{ lat: alert.latitude, lng: alert.longitude }}
+            title={`Alert: ${alert.alerttype}`} // Puedes cambiar el título al detalle de la alerta
+            onClick={() => handleMarkerClick({ lat: alert.latitude, lng: alert.longitude })} // Llama a la función handleMarkerClick cuando se hace clic
           />
         ))}
       </GoogleMap>
